cede1b328375b3bb5caf165d0ed8b87266ce8831
Yusuf Tosin Azeez <yusuftosco66@gmail.com>
2021-01-02 13:39:26 +0100

    Add files via upload

---
+++ ellu.js
@@ -0,0 +1,94 @@
+
+
+var slideIndex =1;
+var timer = null;
+showSlides(slideIndex);
+
+function plusSlides(n) {
+  clearTimeout(timer);
+  showSlides(slideIndex +=n);
+}
+function currentSlide(n) {
+  clearTimeout(timer);
+  showSlides(slideIndex = n);
+}
+function showSlides(n) {
+  var i;
+  var slides = document.getElementsByClassName("myslides");
+  var dots = document.getElementsByClassName("dot");
+  if (n==undefined){n = ++slideIndex}
+  if (n > slides.length) {slideIndex = 1}
+  if (n < 1) {slideIndex = slides.length}
+  for (i = 0; i < slides.length; i++) {
+     slides[i].style.display = "none";
+}
+for (i = 0; i < dots.length; i++) {
+      dots[i].className = dots[i].className.replace(" active", "");
+  }
+  slides[slideIndex-1].style.display = "block";
+  dots[slideIndex-1].className += " active";
+  timer = setTimeout(showSlides, 5000);
+} 
+
+
+function myFunction() {
+  var x = document.getElementById("dropdown-content");
+  if (x.style.display ==="none") {
+      x.style.display = "block";
+  } else {
+      x.style.display ="none";
+  }
+}
+
+// $('.top-arrow').on('click', function(e) {
+//   $( "#open" ).slideToggle( "slow", function() {
+//       // Animation complete.
+//   });
+//   $(this).toggleClass("menu-up menu-down")<i class="fas fa-angle-up"></i>
+//   e.preventDefault();
+// });
+
+$('.top-arrow').click(function() {
+  $( "#open" ).slideToggle( "slow", function() {
+        // Animation complete.
+    });
+  $("i", this).toggleClass("fas fa-angle-up fas fa-angle-down");
+});
+
+  const scrollToTopButton = document.getElementById('js-top');
+  
+  // Let's set up a function that shows our scroll-to-top button if we scroll beyond the height of the initial window.
+  const scrollFunc = () => {
+    // Get the current scroll value
+    let y = window.scrollY;
+    
+    // If the scroll value is greater than the window height, let's add a class to the scroll-to-top button to show it!
+    if (y > 0) {
+      scrollToTopButton.className = "top-link show";
+    } else {
+      scrollToTopButton.className = "top-link hide";
+    }
+  };
+  
+  window.addEventListener("scroll", scrollFunc);
+  
+  const scrollToTop = () => {
+    // Let's set a variable for the number of pixels we are from the top of the document.
+    const c = document.documentElement.scrollTop || document.body.scrollTop;
+    
+    // If that number is greater than 0, we'll scroll back to 0, or the top of the document.
+    // We'll also animate that scroll with requestAnimationFrame:
+    // https://developer.mozilla.org/en-US/docs/Web/API/window/requestAnimationFrame
+    if (c > 0) {
+      window.requestAnimationFrame(scrollToTop);
+      // ScrollTo takes an x and a y coordinate.
+      // Increase the '10' value to get a smoother/slower scroll!
+      window.scrollTo(0, c - c / 10);
+    }
+  };
+  
+  // When the button is clicked, run our ScrolltoTop function above!
+  scrollToTopButton.onclick = function(e) {
+    e.preventDefault();
+    scrollToTop();
+  }
